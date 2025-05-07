80fdfaf54356cab324c6cc06098309123de41b90
Larissa de Santi <desantilarissa@gmail.com>
2025-05-07 12:31:05 -0300

    Create 04_predict_country.sql

---
+++ 04_predict_country.sql
@@ -0,0 +1,20 @@
+-- predict purchases per user
+
+SELECT
+  fullVisitorId,
+  SUM(predicted_label) as total_predicted_purchases -- total predicted purchases for each user
+FROM
+  ml.PREDICT(MODEL `bqml_lab.sample_model`, ( -- apply the trained model for prediction
+SELECT
+  IFNULL(device.operatingSystem, "") AS os,
+  device.isMobile AS is_mobile,
+  IFNULL(totals.pageviews, 0) AS pageviews,
+  IFNULL(geoNetwork.country, "") AS country,
+  fullVisitorId
+FROM
+  `bigquery-public-data.google_analytics_sample.ga_sessions_*`
+WHERE
+  _TABLE_SUFFIX BETWEEN '20170701' AND '20170801'))
+GROUP BY fullVisitorId
+ORDER BY total_predicted_purchases DESC
+LIMIT 10;
